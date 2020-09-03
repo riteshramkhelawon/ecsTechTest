ba0127f5cefc94ec99934e16d7eef498688fca7a
riteshramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-09-03 20:07:59 +0100

    Created helper functions + added comments

--- gradTest.test.js
+++ gradTest.test.js
@@ -1,41 +1,69 @@
 function createMenuData(data) {
+  //separate the data to create one set for parents and another set for children
+  let parentAndChildrenArray = createParentAndChildrenSets(data);
+
+  //use the parent and children sets to create an array of parent-children objects
+  return createParentChildrenObjectArray(parentAndChildrenArray);
+}
+
+//helper functions
+function createParentAndChildrenSets(data){
   //create parent and child sets
   let parentSet = new Set();
-  let childSet = new Set();
-  let resultSet = [];
+  let childrenSet = new Set();
 
-//loop through each element in data
+  //loop through each element in 'data'
   data.forEach(function (currentValue) {
-    //split current string by / and store in array
-    var currentParentAndChild = currentValue.split("/");
+    //split current string element by '/'
+    let currentParentAndChild = currentValue.split("/");
 
-    //store first value in parentArray and second value in childArray
-    //only if there is a child in the currentValue string
+    //if there is a child in the currentValue string
     if (currentParentAndChild.length > 1) {
+      //store the first value of currentParentAndChild array in parentSet
+      //and the second value in childrenSet
       parentSet.add(currentParentAndChild[0]);
-      childSet.add(currentParentAndChild[1]);
+      childrenSet.add(currentParentAndChild[1]);
     }
-
   });
 
+  //return the parent and children sets
+  return [parentSet, childrenSet];
+}
+
+function createParentChildrenObjectArray(parentChildrenSets){
+  //separate parentChildrenSets into parent and children sets
+  let parentSet = parentChildrenSets[0];
+  let childrenSet = parentChildrenSets[1];
+
+  //create an array to store the final result (array of objects)
+  let result =[]
+
+  //for each element in parent set
   parentSet.forEach(function (currentParent) {
-    var newParentObject = {
+    //create new parent object with properties title and data (array)
+    let newParentObject = {
       title: currentParent,
       data: []
-    }
+    };
 
-    childSet.forEach(function (currentChild) {
+    //loop through the children set and find the corresponding children for the current parent
+    childrenSet.forEach(function (currentChild) {
+      //if child element contains the same string as the parent element
       if (currentChild.includes(currentParent)) {
+        //add children to parent object
         newParentObject.data.push(currentChild);
       }
     })
 
-    resultSet.push(newParentObject);
-
+    //push the parent object to the results array
+    result.push(newParentObject);
   })
 
-  return resultSet;
+  //return the array of parent-children objects
+  return result;
 }
+
+
 
 describe("menu Data Generator", () => {
     it("creates correct data structure ", () => {
