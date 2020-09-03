a76dd35eb1bb2e10653a91f235ccee71c0de6726
riteshramkhelawon <ritesh.ramkhelawon@outlook.com>
2020-09-03 17:51:23 +0100

    Implemented solution using sets - test passing

--- gradTest.test.js
+++ gradTest.test.js
@@ -1,5 +1,40 @@
 function createMenuData(data) {
+  //create parent and child sets
+  let parentSet = new Set();
+  let childSet = new Set();
+  let resultSet = [];
 
+//loop through each element in data
+  data.forEach(function (currentValue) {
+    //split current string by / and store in array
+    var currentParentAndChild = currentValue.split("/");
+
+    //store first value in parentArray and second value in childArray
+    //only if there is a child in the currentValue string
+    if (currentParentAndChild.length > 1) {
+      parentSet.add(currentParentAndChild[0]);
+      childSet.add(currentParentAndChild[1]);
+    }
+
+  });
+
+  parentSet.forEach(function (currentParent) {
+    var newParentObject = {
+      title: currentParent,
+      data: []
+    }
+
+    childSet.forEach(function (currentChild) {
+      if (currentChild.includes(currentParent)) {
+        newParentObject.data.push(currentChild);
+      }
+    })
+
+    resultSet.push(newParentObject);
+
+  })
+
+  return resultSet;
 }
 
 describe("menu Data Generator", () => {
